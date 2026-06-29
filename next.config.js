59ceb3ed376577932bc8b291b939decedb0f7912
fakhriaz <fakhriaz@pop-os.tailedef1a.ts.net>
2026-06-29 10:22:23 +0700

    fix: disable eslint during build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 module.exports = nextConfig;
